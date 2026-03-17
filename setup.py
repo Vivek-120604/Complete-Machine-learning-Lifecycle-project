796239b5776e7dc8663a9c75e1708c30c50f06ac
Singh1206 <iamvivekkumar12@gmail.com>
2026-03-17 23:52:20 +0530

    message

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="1.0.0",
+    author="Vivek Kumar Singh",
+    author_email="singhvivek12576@gmail.com",
+    packages=find_packages()
+)
