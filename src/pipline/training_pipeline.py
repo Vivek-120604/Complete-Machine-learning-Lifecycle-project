cd0914f788fc47aaa05f664f8ca27bb95eed2210
Singh1206 <iamvivekkumar12@gmail.com>
2026-07-24 11:51:47 +0530

    added traininng_ pipeline and main_utils

--- src/pipline/training_pipeline.py
+++ src/pipline/training_pipeline.py
@@ -1,146 +1,150 @@
+import json
 import sys
+import os
+
+import pandas as pd
+
+from pandas import DataFrame
+
 from src.exception import MyException
 from src.logger import logging
-
-from src.components.data_ingestion import DataIngestion
-# from src.components.data_validation import DataValidation
-# from src.components.data_transformation import DataTransformation
-# from src.components.model_trainer import ModelTrainer
-# from src.components.model_evaluation import ModelEvaluation
-# from src.components.model_pusher import ModelPusher
-
-from src.entity.config_entity import (DataIngestionConfig)
-                                        #   DataValidationConfig,
-                                        #   DataTransformationConfig,
-                                        #   ModelTrainerConfig,
-                                        #   ModelEvaluationConfig,
-                                        #   ModelPusherConfig)
-                                          
-from src.entity.artifact_entity import (DataIngestionArtifact)
-#                                             DataValidationArtifact,
-#                                             DataTransformationArtifact,
-#                                             ModelTrainerArtifact,
-#                                             ModelEvaluationArtifact,
-#                                             ModelPusherArtifact)
+from src.utils.main_utils import read_yaml_file
+from src.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from src.entity.config_entity import DataValidationConfig
+from src.constants import SCHEMA_FILE_PATH
 
 
-
-class TrainPipeline:
-    def __init__(self):
-        self.data_ingestion_config = DataIngestionConfig()
-        # self.data_validation_config = DataValidationConfig()
-        # self.data_transformation_config = DataTransformationConfig()
-        # self.model_trainer_config = ModelTrainerConfig()
-        # self.model_evaluation_config = ModelEvaluationConfig()
-        # self.model_pusher_config = ModelPusherConfig()
-
-
-    
-    def start_data_ingestion(self) -> DataIngestionArtifact:
+class DataValidation:
+    def __init__(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_config: DataValidationConfig):
         """
-        This method of TrainPipeline class is responsible for starting data ingestion component
+        :param data_ingestion_artifact: Output reference of data ingestion artifact stage
+        :param data_validation_config: configuration for data validation
         """
         try:
-            logging.info("Entered the start_data_ingestion method of TrainPipeline class")
-            logging.info("Getting the data from mongodb")
-            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
-            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-            logging.info("Got the train_set and test_set from mongodb")
-            logging.info("Exited the start_data_ingestion method of TrainPipeline class")
-            return data_ingestion_artifact
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.data_validation_config = data_validation_config
+            self._schema_config =read_yaml_file(file_path=SCHEMA_FILE_PATH)
         except Exception as e:
-            raise MyException(e, sys) from e
-        
-    # def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
-    #     """
-    #     This method of TrainPipeline class is responsible for starting data validation component
-    #     """
-    #     logging.info("Entered the start_data_validation method of TrainPipeline class")
+            raise MyException(e,sys)
 
-    #     try:
-    #         data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
-    #                                          data_validation_config=self.data_validation_config
-    #                                          )
-
-    #         data_validation_artifact = data_validation.initiate_data_validation()
-
-    #         logging.info("Performed the data validation operation")
-    #         logging.info("Exited the start_data_validation method of TrainPipeline class")
-
-    #         return data_validation_artifact
-    #     except Exception as e:
-    #         raise MyException(e, sys) from e
-        
-    # def start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
-    #     """
-    #     This method of TrainPipeline class is responsible for starting data transformation component
-    #     """
-    #     try:
-    #         data_transformation = DataTransformation(data_ingestion_artifact=data_ingestion_artifact,
-    #                                                  data_transformation_config=self.data_transformation_config,
-    #                                                  data_validation_artifact=data_validation_artifact)
-    #         data_transformation_artifact = data_transformation.initiate_data_transformation()
-    #         return data_transformation_artifact
-    #     except Exception as e:
-    #         raise MyException(e, sys)
-        
-    # def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
-    #     """
-    #     This method of TrainPipeline class is responsible for starting model training
-    #     """
-    #     try:
-    #         model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
-    #                                      model_trainer_config=self.model_trainer_config
-    #                                      )
-    #         model_trainer_artifact = model_trainer.initiate_model_trainer()
-    #         return model_trainer_artifact
-
-    #     except Exception as e:
-    #         raise MyException(e, sys)
-
-    # def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
-    #                            model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
-    #     """
-    #     This method of TrainPipeline class is responsible for starting modle evaluation
-    #     """
-    #     try:
-    #         model_evaluation = ModelEvaluation(model_eval_config=self.model_evaluation_config,
-    #                                            data_ingestion_artifact=data_ingestion_artifact,
-    #                                            model_trainer_artifact=model_trainer_artifact)
-    #         model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
-    #         return model_evaluation_artifact
-    #     except Exception as e:
-    #         raise MyException(e, sys)
-
-    # def start_model_pusher(self, model_evaluation_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
-    #     """
-    #     This method of TrainPipeline class is responsible for starting model pushing
-    #     """
-    #     try:
-    #         model_pusher = ModelPusher(model_evaluation_artifact=model_evaluation_artifact,
-    #                                    model_pusher_config=self.model_pusher_config
-    #                                    )
-    #         model_pusher_artifact = model_pusher.initiate_model_pusher()
-    #         return model_pusher_artifact
-    #     except Exception as e:
-    #         raise MyException(e, sys)
-
-    def run_pipeline(self, ) -> None:
+    def validate_number_of_columns(self, dataframe: DataFrame) -> bool:
         """
-        This method of TrainPipeline class is responsible for running complete pipeline
+        Method Name :   validate_number_of_columns
+        Description :   This method validates the number of columns
+        
+        Output      :   Returns bool value based on validation results
+        On Failure  :   Write an exception log and then raise an exception
         """
         try:
-            data_ingestion_artifact = self.start_data_ingestion()
-            # data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-            # data_transformation_artifact = self.start_data_transformation(
-            #     data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
-            # model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
-            # model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
-            #                                                         model_trainer_artifact=model_trainer_artifact)
-            # if not model_evaluation_artifact.is_model_accepted:
-            #     logging.info(f"Model not accepted.")
-            #     return None
-            # model_pusher_artifact = self.start_model_pusher(model_evaluation_artifact=model_evaluation_artifact)
-            
+            status = len(dataframe.columns) == len(self._schema_config["columns"])
+            logging.info(f"Is required column present: [{status}]")
+            return status
         except Exception as e:
             raise MyException(e, sys)
+
+    def is_column_exist(self, df: DataFrame) -> bool:
+        """
+        Method Name :   is_column_exist
+        Description :   This method validates the existence of a numerical and categorical columns
+        
+        Output      :   Returns bool value based on validation results
+        On Failure  :   Write an exception log and then raise an exception
+        """
+        try:
+            dataframe_columns = df.columns
+            missing_numerical_columns = []
+            missing_categorical_columns = []
+            for column in self._schema_config["numerical_columns"]:
+                if column not in dataframe_columns:
+                    missing_numerical_columns.append(column)
+
+            if len(missing_numerical_columns)>0:
+                logging.info(f"Missing numerical column: {missing_numerical_columns}")
+
+
+            for column in self._schema_config["categorical_columns"]:
+                if column not in dataframe_columns:
+                    missing_categorical_columns.append(column)
+
+            if len(missing_categorical_columns)>0:
+                logging.info(f"Missing categorical column: {missing_categorical_columns}")
+
+            return False if len(missing_categorical_columns)>0 or len(missing_numerical_columns)>0 else True
+        except Exception as e:
+            raise MyException(e, sys) from e
+
+    @staticmethod
+    def read_data(file_path) -> DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise MyException(e, sys)
+        
+
+    def initiate_data_validation(self) -> DataValidationArtifact:
+        """
+        Method Name :   initiate_data_validation
+        Description :   This method initiates the data validation component for the pipeline
+        
+        Output      :   Returns bool value based on validation results
+        On Failure  :   Write an exception log and then raise an exception
+        """
+
+        try:
+            validation_error_msg = ""
+            logging.info("Starting data validation")
+            train_df, test_df = (DataValidation.read_data(file_path=self.data_ingestion_artifact.trained_file_path),
+                                 DataValidation.read_data(file_path=self.data_ingestion_artifact.test_file_path))
+
+            # Checking col len of dataframe for train/test df
+            status = self.validate_number_of_columns(dataframe=train_df)
+            if not status:
+                validation_error_msg += f"Columns are missing in training dataframe. "
+            else:
+                logging.info(f"All required columns present in training dataframe: {status}")
+
+            status = self.validate_number_of_columns(dataframe=test_df)
+            if not status:
+                validation_error_msg += f"Columns are missing in test dataframe. "
+            else:
+                logging.info(f"All required columns present in testing dataframe: {status}")
+
+            # Validating col dtype for train/test df
+            status = self.is_column_exist(df=train_df)
+            if not status:
+                validation_error_msg += f"Columns are missing in training dataframe. "
+            else:
+                logging.info(f"All categorical/int columns present in training dataframe: {status}")
+
+            status = self.is_column_exist(df=test_df)
+            if not status:
+                validation_error_msg += f"Columns are missing in test dataframe."
+            else:
+                logging.info(f"All categorical/int columns present in testing dataframe: {status}")
+
+            validation_status = len(validation_error_msg) == 0
+
+            data_validation_artifact = DataValidationArtifact(
+                validation_status=validation_status,
+                message=validation_error_msg,
+                validation_report_file_path=self.data_validation_config.validation_report_file_path
+            )
+
+            # Ensure the directory for validation_report_file_path exists
+            report_dir = os.path.dirname(self.data_validation_config.validation_report_file_path)
+            os.makedirs(report_dir, exist_ok=True)
+
+            # Save validation status and message to a JSON file
+            validation_report = {
+                "validation_status": validation_status,
+                "message": validation_error_msg.strip()
+            }
+
+            with open(self.data_validation_config.validation_report_file_path, "w") as report_file:
+                json.dump(validation_report, report_file, indent=4)
+
+            logging.info("Data validation artifact created and saved to JSON file.")
+            logging.info(f"Data validation artifact: {data_validation_artifact}")
+            return data_validation_artifact
+        except Exception as e:
+            raise MyException(e, sys) from e
